167db24eacab9605c48161eac3026bdc49f7b260
dreruff <gregoryaruffin@gmail.com>
2017-02-17 00:16:54 -0800

    converted all html about pages to twig and set up routes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,73 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/greg", name="greg")
+     */
+    public function gregAction()
+    {
+        return $this->render('default/aboutgreg.html.twig');
+    }
+
+    /**
+     * @Route("/alex", name="alex")
+     */
+    public function alexAction()
+    {
+        return $this->render('default/aboutalex.html.twig');
+    }
+
+    /**
+     * @Route("/anthony", name="anthony")
+     */
+    public function anthonyAction()
+    {
+        return $this->render('default/aboutanthony.html.twig');
+    }
+
+    /**
+     * @Route("/robin", name="robin")
+     */
+    public function robinAction()
+    {
+        return $this->render('default/aboutrobin.html.twig');
+    }
+
+    /**
+     * @Route("/avery", name="avery")
+     */
+    public function averyAction()
+    {
+        return $this->render('default/aboutavery.html.twig');
+    }
+
+    /**
+     * @Route("/leanna", name="leanna")
+     */
+    public function leanneAction()
+    {
+        return $this->render('default/aboutleanna.html.twig');
+    }
+
+    /**
+     * @Route("/elric", name="elric")
+     */
+    public function elricAction()
+    {
+        return $this->render('default/aboutelric.html.twig');
     }
 }
+
