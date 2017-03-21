39baf6e8c2d7aaad241f5013b939966dd665ba06
dreruff <gregoryaruffin@gmail.com>
2017-03-20 17:32:15 -0700

    cleaned up the controller and twig files

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Item;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -80,43 +79,46 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/prototype" , name="search")
+     * @Route("/prototype")
      */
     public function prototypeAction(Request $request)
     {
-	$item =  new Item();
+        $item = new Item();
 
-	$form = $this->createFormBuilder($item)
-		->add('id', IntegerType::class, array('mapped' => false))
-		->add('search', SubmitType::class, array('label' => 'Search Item'))
-		->getForm();
-	$form->handleRequest($request);
+        //Creates the form
+        $form = $this->createFormBuilder($item)
+            ->add('id', IntegerType::class, array('mapped' => false))
+            ->add('search', SubmitType::class, array('label' => 'Search Item'))
+            ->getForm();
 
-	if ($form->isSubmitted() && $form->isValid()) {
-		$key = $form['id']->getData();
+        $form->handleRequest($request);
 
-		$item = $this->getDoctrine()
-			->getRepository('AppBundle:Item')
-                	->find($key);
-		
-		if (!$item) {
-        		throw $this->createNotFoundException(
-            		'No item found for id '.$key);
-		}	
-	
-		$em = $this->getDoctrine()->getManager();
-	        $em->persist($item);
-       		$em->flush();
 
-		return $this->render('default/prototype.html.twig', array('form' => $form->createView(), 'item' => $item));
-		//return $this->redirectToRoute('search');
-	}
-/*	
-        $item = $this->getDoctrine()
-		->getRepository('AppBundle:Item')
-		->find(1);
-*/
-        return $this->render('default/prototype.html.twig', array('form' => $form->createView(), 'item' => $item));
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // $form->getData() holds the submitted values
+            $id = $form['id']->getData();
+
+            $item = $this->getDoctrine()
+                ->getRepository('AppBundle:Item')
+                ->find($id);
+
+            //Handles bounds. Not Completed. Needs validation or better handling action
+            if (!$item) {
+                throw $this->createNotFoundException(
+                    'No item found for id ' . $id);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($item);
+            $em->flush();
+
+            return $this->render('default/prototype.html.twig',
+                array('form' => $form->createView(), 'item' => $item));
+        }
+
+        return $this->render('default/prototype.html.twig',
+            array('form' => $form->createView(), 'item' => $item));
     }
 }
 
