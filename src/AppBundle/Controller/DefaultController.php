ff596bc5262c10d1ee6f227b8618583cb6cd614b
lspangan <lspangan@users.noreply.github.com>
2017-03-31 19:20:47 -0700

    changed indexAction() to render home.html.twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,7 +20,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig');
+        return $this->render('default/home.html.twig');
     }
 
     /**
