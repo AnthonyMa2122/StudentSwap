5c28882949800f09919540d78621a2e41eebc3d0
dreruff <gregoryaruffin@gmail.com>
2017-05-01 16:51:05 -0700

    create form for create post

--- src/AppBundle/Controller/CreatePostController.php
+++ src/AppBundle/Controller/CreatePostController.php
@@ -2,20 +2,54 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Item;
+use Doctrine\DBAL\Types\TextType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\Request;
 
 class CreatePostController extends Controller
 {
     /**
      * @Route("/createPost", name="createPost")
      */
-    public function createPostAction()
+    public function createPostAction(Request $request)
     {
-
+        $item = new Item();
 
         $form = $this->createFormBuilder($item)
+            ->add('title', TextType::class)
+            ->add('description', TextType::class)
+            ->add('category', ChoiceType::class, array(
+                'choices'  => array(
+                    'Book' => "book",
+                    'Tech' => "tech",
+                    'Clothes' => "clothes")
+            ))
+            ->add('condition', ChoiceType::class, array(
+                'choices'  => array(
+                    'New' => "new",
+                    'Good' => "good",
+                    'Poor' => "poor")
+            ));
+            //->add('Image')
 
-        return $this->render('default/createPost.html.twig');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // $form->getData() holds the submitted values
+            $item = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($item);
+            $em->flush();
+
+            return $this->render('default/home.html.twig');
+        }
+
+
+        return $this->render('default/createPost.html.twig', array('form' => $form->createView()));
     }
 }
