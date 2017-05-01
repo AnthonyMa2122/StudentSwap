5d58a9e532299ae5d67b1facce869ca5424a9921
elric12 <elricd@ip-172-31-19-160.us-west-1.compute.internal>
2017-05-01 09:16:55 +0000

    Added image upload prototype.

---
+++ src/AppBundle/Controller/UploadController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Images;
+use AppBundle\Form\ImagesType;
+use Vich\UploaderBundle\Naming\NamerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class UploadController extends Controller 
+{
+	
+	/**
+	 * @Route("/upload/{up}")
+	 */
+	public function showAction($up) 
+	{
+		return $this->render ( 'default/upload.html.twig' );
+	}
+	
+	/**
+	 * @Route("/uploader", name="uploader")
+	 */
+	public function newAction2(Request $request) 
+	{
+		$images = new Images ();
+		$form = $this->createForm ( ImagesType::class, $images );
+		$form->handleRequest ( $request );
+		
+		if ($form->isSubmitted () && $form->isValid ()) 
+		{
+			$file = $images->getImageFile ();
+			
+			$em = $this->getDoctrine ()->getManager ();
+			$em->persist ( $images );
+			$em->flush ();
+			
+			return $this->redirect ( $this->generateUrl ( 'homepage' ) );
+		}
+		
+		return $this->render ( 'default/uploader.html.twig', array (
+				'form' => $form->createView () 
+		) );
+	}
+	
+	/**
+	 * @Route("/uploadershow/{b}", name="uploadshow")
+	 */
+	public function uploadshow(Request $a, $b) 
+	{
+		$form = $this->createFormBuilder ()->add ( 'get', TextType::class )->getForm ();
+		
+		$form2 = $this->createFormBuilder ()->add ( 'remove', TextType::class )->getForm ();
+		
+		$form->handleRequest ( $a );
+		$form2->handleRequest ( $a );
+		
+		if ($form->isSubmitted () && $form->isValid ()) 
+		{
+			
+			$val = $form ["get"]->getData ();
+			$product = $this->getDoctrine ()->getRepository ( 'AppBundle:Images' )->find ( $val );
+			
+			if (! $val || ! $product) 
+			{
+				throw $this->createNotFoundException ( 'No product found for id ' . $val );
+			}
+			
+			return $this->render ( 'default/uploadershow.html.twig', array (
+					'li' => $this->container->get ( 'vich_uploader.templating.helper.uploader_helper' )->asset ( $product, "imageFile" ),
+					'form' => $form->createView (),
+					'form2' => $form2->createView () 
+			) );
+		}
+		
+		if ($form2->isSubmitted () && $form2->isValid ()) 
+		{
+			
+			$val = $form2 ["remove"]->getData ();
+			$product = $this->getDoctrine ()->getRepository ( 'AppBundle:Images' )->find ( $val );
+			
+			if (! $val || ! $product) 
+			{
+				throw $this->createNotFoundException ( 'No product found for id ' . $val );
+			}
+			
+			$em = $this->getDoctrine ()->getManager ();
+			$em->remove ( $product );
+			$em->flush ();
+			
+			return $this->render ( 'default/uploadershow.html.twig', array (
+					'li' => 'deleted ' . $val,
+					'form' => $form->createView (),
+					'form2' => $form2->createView () 
+			) );
+		}
+		
+		if ($b > 0) 
+		{
+			$val = $b;
+			$product = $this->getDoctrine ()->getRepository ( 'AppBundle:Images' )->find ( $val );
+			
+			if (! $val || ! $product) 
+			{
+				throw $this->createNotFoundException ( 'No product found for id ' . $val );
+			}
+		} 
+		else 
+		{
+			return $this->render ( 'default/uploadershow.html.twig', array (
+					'li' => 'uploads/images/',
+					'form' => $form->createView (),
+					'form2' => $form2->createView () 
+			) );
+		}
+		
+		return $this->render ( 'default/uploadershow.html.twig', array (
+				'li' => 'uploads/images/' . $product->getImageName (),
+				'form' => $form->createView (),
+				'form2' => $form2->createView () 
+		) );
+	}
+	
+}
---
+++ src/AppBundle/Entity/Images.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @Vich\Uploadable
+ */
+class Images 
+{
+	
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+		
+	/**
+	 * @Assert\NotBlank(message="Please upload a png or jpg.")
+	 * @Assert\File(mimeTypes={ "image/png", "image/jpeg" })
+	 * @Vich\UploadableField(mapping="product_image", fileNameProperty="imageName", size="imageSize")
+	 */
+	private $imageFile;
+	
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $imageName;
+
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $updatedAt;
+	
+	/**
+	 * @return Images
+	 */
+	public function setImageFile(File $image = null) 
+	{
+		$this->imageFile = $image;
+		
+		if ($image) 
+		{
+			$this->updatedAt = new \DateTimeImmutable ();
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * @return File|null
+	 */
+	public function getImageFile() 
+	{
+		return $this->imageFile;
+	}
+	
+	/**
+	 * @param string $imageName
+	 *
+	 * @return Images
+	 */
+	public function setImageName($imageName) 
+	{
+		$this->imageName = $imageName;
+		
+		return $this;
+	}
+	
+	/**
+	 * @return string|null
+	 */
+	public function getImageName() 
+	{
+		return $this->imageName;
+	}
+	
+}
