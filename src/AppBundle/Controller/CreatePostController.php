d50893c391873f17f4c1ec25d87fd993744febf9
dreruff <gregoryaruffin@gmail.com>
2017-05-02 12:59:42 -0700

    added securityto open orders and create post

--- src/AppBundle/Controller/CreatePostController.php
+++ src/AppBundle/Controller/CreatePostController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Item;
 
+use FOS\UserBundle\Model\UserInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Validator\Constraints\DateTime;
 
 class CreatePostController extends Controller
@@ -20,6 +22,12 @@ class CreatePostController extends Controller
      */
     public function createPostAction(Request $request)
     {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+
         $item = new Item();
 
         $form = $this->createFormBuilder($item)
