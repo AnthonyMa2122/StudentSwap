0ad0c333c0058c46c0b069e59d7e4dcc40a33acf
agaesser <agaesser@gmail.com>
2017-05-05 20:32:34 +0000

    added return value and comments

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,4 +1,10 @@
+/**
+ * you can access this value with:
+ * document.getElementById("categoryFilter").innerHTML = getCategory;
+ * */
+
 
 function getCategory(category) {
     console.log(category[category.selectedIndex].value); //get value
+    return category[category.selectedIndex].value;
 }
